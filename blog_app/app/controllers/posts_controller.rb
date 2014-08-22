5b089970696d61d27a84af1a801c2109a664de5b
rasbabbo <rasbabbo@yahoo.com>
2014-08-21 20:47:57 -0700

    closer

--- blog_app/app/controllers/posts_controller.rb
+++ blog_app/app/controllers/posts_controller.rb
@@ -12,18 +12,18 @@ class PostsController < ApplicationController
 		new_post = params[:post].permit(:title, :description, :author)
 		post_holder = Post.create(new_post)
 
-		new_tag = params[:tag].permit(:name)
+		# new_tag = params[:tag].permit(:name)
 
-		if new_tag["name"] != nil
-			exists = Tag.find_by_name(new_tag["name"])
-				if exists ==  nil
-					tag_holder = Tag.create(new_tag)
-					post_holder.tags << tag_holder
+		# if new_tag["name"] != nil
+		# 	exists = Tag.find_by_name(new_tag["name"])
+		# 		if exists ==  nil
+		# 			tag_holder = Tag.create(new_tag)
+		# 			post_holder.tags << tag_holder
 
-				else
-					post_holder.tags << exists
-				end
-			end
+		# 		else
+		# 			post_holder.tags << exists
+		# 		end
+		# 	end
 
 		redirect_to "/posts"
 	end
